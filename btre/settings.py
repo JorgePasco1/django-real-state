44c02ae4b7fcbb260e53c81042757614022945a9
jorgepasco1 <jorgepascosoto@gmail.com>
2022-03-13 22:47:28 -0500

    finish inquiry functionality

--- btre/settings.py
+++ btre/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -150,3 +151,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('HOST_PASSWORD')
+EMAIL_USE_TLS = True
---
+++ contacts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from contacts import views
+
+urlpatterns = [
+    path('contact', views.contact, name='contact')
+]
